1000f95d2a9ebf25718e45d3213ffbc5cf6a5fab
iZHpy <929784745@qq.com>
2025-10-06 13:39:18 +0000

    modify thre/remove useless code

--- search_params.py
+++ search_params.py
@@ -123,17 +123,37 @@ def objective(trial):
         'n_layers_sample_enc': trial.suggest_int('n_layers_sample_enc', 2, 3, 5),
         'n_layers_label_enc': trial.suggest_int('n_layers_label_enc', 2, 3, 5),
         'n_head': trial.suggest_categorical('n_head', [4, 8]),
-        'sample_enc_dropout': trial.suggest_float('sample_enc_dropout', 0.0, 0.5),
-        'label_enc_dropout': trial.suggest_float('label_enc_dropout', 0.0, 0.5),
-        'lr': trial.suggest_float('lr', 2e-5, 1e-3, log=True),
-        'eta_min': trial.suggest_float('eta_min', 1e-6, 1e-5, log=True),
+        'sample_enc_dropout': trial.suggest_float("sample_enc_dropout", 0.0, 0.5, step=0.1),
+        'label_enc_dropout': trial.suggest_float('label_enc_dropout', 0.0, 0.5, step=0.1),
+        'lr': trial.suggest_categorical('lr', [2e-5, 5e-5, 1e-4, 2e-4, 5e-4, 8e-4, 1e-3]),
+        'eta_min': trial.suggest_categorical('eta_min', [1e-6, 5e-6, 1e-5]),
         'T0': trial.suggest_int('T0', 1, 2, 4),
         'T_mult': trial.suggest_categorical('T_mult', [2, 4]),
-        'nll_e_weight': trial.suggest_float('nll_e_weight', 1.0, 6.0),
-        'nll_x_weight': trial.suggest_float('nll_x_weight', 1.0, 8.0),
-        'kl_weight': trial.suggest_float('kl_weight', 0, 1),
-        'cpc_weight': trial.suggest_float('cpc_weight', 0, 1),
+        'nll_e_weight': trial.suggest_float('nll_e_weight', 1.0, 6.0, step=1),
+        'nll_x_weight': trial.suggest_float('nll_x_weight', 1.0, 8.0, step=1),
+        'kl_weight': trial.suggest_float('kl_weight', 0, 1, step=0.2),
+        'cpc_weight': trial.suggest_float('cpc_weight', 0, 1, step=0.2),
     }
+
+    # params = {
+    #     'd_model': trial.suggest_categorical('d_model', [64, 128, 256, 512]),
+    #     'd_inner_hid': trial.suggest_categorical('d_inner_hid', [256, 512, 1024, 2048]),
+    #     'batch_size': trial.suggest_categorical('batch_size', [32, 64, 128]),
+    #     'n_layers_sample_enc': trial.suggest_int('n_layers_sample_enc', 2, 3, 5),
+    #     'n_layers_label_enc': trial.suggest_int('n_layers_label_enc', 2, 3, 5),
+    #     'n_head': trial.suggest_categorical('n_head', [4, 8]),
+    #     'sample_enc_dropout': trial.suggest_float('sample_enc_dropout', 0.0, 0.5),
+    #     'label_enc_dropout': trial.suggest_float('label_enc_dropout', 0.0, 0.5),
+    #     'lr': trial.suggest_float('lr', 2e-5, 1e-3, log=True),
+    #     'eta_min': trial.suggest_float('eta_min', 1e-6, 1e-5, log=True),
+    #     'T0': trial.suggest_int('T0', 1, 2, 4),
+    #     'T_mult': trial.suggest_categorical('T_mult', [2, 4]),
+    #     'nll_e_weight': trial.suggest_float('nll_e_weight', 1.0, 6.0),
+    #     'nll_x_weight': trial.suggest_float('nll_x_weight', 1.0, 8.0),
+    #     'kl_weight': trial.suggest_float('kl_weight', 0, 1),
+    #     'cpc_weight': trial.suggest_float('cpc_weight', 0, 1),
+    # }
+
     metric = train_eval(params, opt=opt, trial=trial)
     return metric
 
